6450466c36c2565b23b596de18a123d997757c47
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-02-13 11:24:56 -0400

    Usando Location para redireccionamiento directo

--- verifica.php
+++ verifica.php
@@ -3,10 +3,12 @@
     $password=$_GET["password"];
     if ($username =="jorge" && $password =="12345678") {
         $_SESSION["id_user"]=100;
-        echo "<a href = 'saludo.php'> Saludo</a>";
+        header("Location: saludo.php"); // Redirecionamiento directo
+        // echo "<a href = 'saludo.php'> Saludo</a>";
     }
     else {
-        echo "<a href = 'index.htm'>No valido</a>";
+        header("Location: index.htm"); // Redirecionamiento directo
+        // echo "<a href = 'index.htm'>No valido</a>";
     }
-    // Para habilitar las sesiones Linux necesista autorizar, en windows abrimos el archivo php.ini colocamos session.auto_start=0, para habilitar las sessiones, posterirmente lo reinicias apache en XAMP
+    // Para habilitar las sesiones Linux necesista autorizar, en windows abrimos el archivo php.ini colocamos session.auto_start=0 (debes colocar la session_start();), para habilitar las sessiones, posterirmente lo reinicias apache en XAMP. Si colocas session.auto_start=1 no es necesario iniciar la sesion.
     ?>
